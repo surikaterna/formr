4e4cecb68f67bced8489a4c222be26498da4125a
Andreas Karlsson <sprawl@onlajn.nu>
2016-07-19 23:13:48 +0200

    Started to add schema

---
+++ src/schema/schema.js
@@ -0,0 +1,11 @@
+/**
+ * Support us in the nine circles of hell which is json-schema
+ */
+export default class Schema {
+  constructor(jsonSchema) {
+    this._schema = jsonSchema;
+  }
+  getTypeByPath(path) {
+
+  }
+}
---
+++ test/schema/schema.js
@@ -0,0 +1,9 @@
+import should from 'should';
+import Schema from '../../src/schema';
+
+describe('Schema', () => {
+  describe('#getTypeByPath', () => {
+    it('should return string type', () => {
+    });
+  });
+});
