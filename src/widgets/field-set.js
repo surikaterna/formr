548ce84999376270bd7b27642360eb0369771c4b
Andreas Karlsson <sprawl@onlajn.nu>
2016-09-08 00:11:14 +0200

    Started playing with material design and pure css

---
+++ src/widgets/field-set.js
@@ -0,0 +1,6 @@
+import React from 'react';
+export default function FieldSet(props) {
+  return <div>
+            FIELDSET
+        </div>;
+}
---
+++ src/widgets/material/input-text.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import BaseInput from './base-input';
+
+export default function InputText(props) {
+  return <BaseInput {...props} type="text"/>;
+}
