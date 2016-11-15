c1df6a1b616c0b3dc8af2e6c5f68d862539b099c
Andreas Karlsson <sprawl@onlajn.nu>
2016-11-15 19:20:08 +0100

    Started to add core providers to "service"

--- src/fields/patterned-field.js
+++ src/fields/patterned-field.js
@@ -31,7 +31,23 @@ const _componentConverter = {
 const _getWidgetFromType = (type) => {
   const fieldType = _componentConverter[type] || type;
   return this.props.$formr.componentFactory(fieldType);
-}
+};
 
 // if props.type = 'json-schema-type' resolve to correct component type
-PatternService.addProvider((schema, props) => (props.type && typeof (props.type) === 'string') ? _getWidgetFromType(props.type));
+PatternService.addProvider((schema, props) => (props.type && typeof (props.type) === 'string') ? _getWidgetFromType(props.type) : undefined);
+
+  /**
+   * if props.type = 'function' use it as widget
+   * if props.type = 'json-schema-type' resolve to correct component type
+   * else if props.type = 'component-type' resolve component
+   *
+   * USER DEFINED PROVIDERS ??
+   * <Component id='Aloha' matches={$ref:'#/definitions/orders/test'}>
+   *  <div>Hello World!</div> 
+   * </Component>
+   * <Aloha value={'adsasdsd'} />
+   * else if no props.type but props.value is an expression,
+   * take path from expression and resolve the corresponding type from schema
+   * else use type = string
+   *
+   */
