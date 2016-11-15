747a4df3b454553cb71b51d271863cbda4b5a228
Andreas Karlsson <sprawl@onlajn.nu>
2016-11-15 01:05:40 +0100

    Default state + remove logging

--- src/fields/state-field.js
+++ src/fields/state-field.js
@@ -5,10 +5,10 @@ export default (FormComponent) => class FieldWithState extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pristine: true,
-      touched: false,
-      visited: false,
-      valid: true,
+      pristine: props.pristine || true,
+      touched: props.touched || false,
+      visited: props.visited || false,
+      valid: props.valid || true,
       errors: []
     };
   }
@@ -17,14 +17,10 @@ export default (FormComponent) => class FieldWithState extends Component {
     if (this.props.$formr && this.props.$formr.schema) {
       const validator = new Validator(this.props.$formr.schema);
       errors = validator.validate(val);
-      console.log('VAL', errors);
-    } else {
-      console.log('NO VAL ', this.props);
     }
     this.setState({ errors });
   }
   render() {
-    console.log('STATE', this.state, this.props);
     return <FormComponent {...this.props}
       onFocus={() => this.setState({ visited: true, active: true })}
       onBlur={() => { this.setState({ touched: true, active: false }); this._validate(this.props.value); } }
